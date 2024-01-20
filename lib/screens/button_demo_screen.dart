421ddc6fd055f7bf4c7798485e5c62257331469d
Abid <abidroid1@gmail.com>
2024-01-20 14:56:44 +0500

    button demo screen added
    we will cover button here

---
+++ lib/screens/button_demo_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ButtonDemoScreen extends StatelessWidget {
+  const ButtonDemoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buttons'),
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
